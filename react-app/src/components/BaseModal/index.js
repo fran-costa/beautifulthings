2be4495e45680df8d9d4d6fa24046920158926e5
Francisco Costa <francisco.costa@unixono.com>
2018-06-22 16:23:19 -0300

    Improve BaseModal to support left modals

--- react-app/src/components/BaseModal/index.js
+++ react-app/src/components/BaseModal/index.js
@@ -11,18 +11,28 @@ export default class BaseModal extends PureComponent {
     visible: PropTypes.bool.isRequired,
 
     /**
+     * Whenever the modal is displayed on the left or if
+     * is displayed as a floating modal (default behaviour)
+     */
+    leftModal: PropTypes.bool,
+
+    /**
      * The element to use as the modal content
      */
     children: PropTypes.element.isRequired
   };
 
+  static defaultProps = { leftModal: false }
+
   render() {
     if (!this.props.visible) return null;
+
+    const modalStyle = this.props.leftModal ? styles.leftModal : styles.floattingModal;
 
     return (
       <div>
         <div className={styles.opacityLayer} />
-        <div className={styles.modal}>
+        <div className={modalStyle}>
           {this.props.children}
         </div>
       </div>
