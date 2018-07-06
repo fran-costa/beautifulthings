e60bf062193f0da426d23d39563d5b55b2bf36cb
Francisco Costa <francisco.costa@unixono.com>
2018-07-06 14:40:35 -0300

    Add ListScreenWrapper to AppRouter

--- react-app/src/containers/AppRouter/index.js
+++ react-app/src/containers/AppRouter/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
 
 import EditScreenWrapper from 'containers/EditScreenWrapper';
+import ListScreenWrapper from 'containers/ListScreenWrapper';
 import SignUpScreen from 'containers/SignUpScreen';
 import StartScreen from 'containers/StartScreen';
 
 class AppRouter extends React.PureComponent {
   _showStartScreen = () => window.location.hash = '/start';
   _showSignUpScreen = () => window.location.hash = '/signup';
-  _showListScreen = () => window.location.hash = 'list';
+  _showAddScreen = () => window.location.hash = '/add';
+  _showEditScreen = entryDate => window.location.hash = `/edit/${entryDate}`;
+  _showListScreen = () => window.location.hash = '/list';
 
   _startScreen = props => <StartScreen
     {...props}
@@ -26,6 +29,12 @@ class AppRouter extends React.PureComponent {
     onSave={() => { /* TODO */ }}
   />
 
+  _listScreen = props => <ListScreenWrapper
+    {...props}
+    onAdd={this._showAddScreen}
+    onEdit={this._showEditScreen}
+  />;
+
   render() {
     return (
       <HashRouter>
@@ -35,6 +44,7 @@ class AppRouter extends React.PureComponent {
           <Route exact path="/signup" render={this._signUpScreen} />
           <Route exact path="/edit" render={this._editScreen} />
           <Route exact path="/edit/:date" render={this._editScreen} />
+          <Route exact path="/list" render={this._listScreen} />
         </div>
       </HashRouter>
     );
