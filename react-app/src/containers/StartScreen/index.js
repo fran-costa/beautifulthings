e01c492c1027c52b1b98ea8df4133a8180a494cd
Francisco Costa <francisco.costa@unixono.com>
2018-07-12 19:04:37 -0300

    Add first approach of StartScreen with styles and refactor

--- react-app/src/containers/StartScreen/index.js
+++ react-app/src/containers/StartScreen/index.js
@@ -1,54 +1,79 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+
+import Account from 'account';
+
+import Button from 'components/Button';
+import InputText from 'components/InputText';
+import Logo from 'components/Logo';
 
 import styles from './index.module.scss';
 
-import Logo from 'components/Logo';
-import Button from 'components/Button';
-import InputText from 'components/InputText';
+export default class StartScreen extends React.PureComponent {
+  state = {
+    username: '',
+    password: '',
+  };
 
-export default class StartScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
+  componentDidMount() {
+    this._usernameInput.focus();
   }
 
-  componentDidMount() { this._usernameInputRef.focus(); }
-
-  _setUsernameInputRef = element => this._usernameInputRef = element;
-  _setPasswordInputRef = element => this._passwordInputRef = element;
+  _setUsernameInputRef = element => this._usernameInput = element;
+  _setPasswordInputRef = element => this._passwordInput = element;
 
   _handleUsernameChange = username => this.setState({ username });
   _handlePasswordChange = password => this.setState({ password });
 
-  _handleUsernameEnter = () => this._passwordInputRef.focus();
+  _handleUsernameEnter = () => this._passwordInput.focus();
 
-  _isUsernameAndPasswordValid = () =>
-    (this.state.username.length > 0 && this.state.password.length > 0) ? true : false;
+  _handleSignIn = () => { /* TODO */ }
+  _handleSignUp = () => { /* TODO */ }
 
   render() {
+    const validUserPass = Account.validateUserPass(this.state.username, this.state.password);
+
     return (
       <div className={styles.container}>
-        <Logo size={Logo.BIG} />
-        <InputText
-          type={InputText.TEXT}
-          label="Username: "
-          placeholder="Username"
-          onChange={this._handleUsernameChange}
-          onEnter={this._handleUsernameEnter}
-          ref={this._setUsernameInputRef}
-        />
-        <InputText
-          type={InputText.PASSWORD}
-          label="Password: "
-          placeholder="Password"
-          onChange={this._handlePasswordChange}
-          ref={this._setPasswordInputRef}
-        />
-        <Button disabled={!this._isUsernameAndPasswordValid()}>Sign In</Button>
-        <Button>Sign Up</Button>
+        <div className={styles.mainContainer}>
+          <div className={styles.logoContainer}>
+            {Logo({ size: Logo.BIG })}
+          </div>
+          <div className={styles.formContainer}>
+            <div className={styles.usernameInputContainer}>
+              <InputText
+                type={InputText.TEXT}
+                label="Username"
+                onChange={this._handleUsernameChange}
+                onEnter={this._handleUsernameEnter}
+                ref={this._setUsernameInputRef}
+              />
+            </div>
+            <div className={styles.passwordInputContainer}>
+              <InputText
+                type={InputText.PASSWORD}
+                label="Password"
+                onChange={this._handlePasswordChange}
+                ref={this._setPasswordInputRef}
+              />
+            </div>
+            <div>
+              <Button
+                disabled={!validUserPass}
+                onClick={this._handleSignIn}
+              >
+                Sign in
+              </Button>
+            </div>
+            <div className={styles.createYourAccountMessage}>
+              - Create your account -
+            </div>
+            <div>
+              <Button onClick={this._handleSignUp}>
+                Sign up
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
