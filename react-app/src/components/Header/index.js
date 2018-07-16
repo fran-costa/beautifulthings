3bb564667131e3cfb5230b356e138893c0569ed6
Francisco Costa <francisco.costa@unixono.com>
2018-07-16 17:53:10 -0300

    Refactor Header component as a functional component

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -4,42 +4,46 @@ import PropTypes from 'prop-types';
 import Logo from 'components/Logo';
 import styles from './index.module.scss';
 
-export default class Header extends React.PureComponent {
-  static propTypes = {
-    /**
-     * The element (an ActionIcon) that this element will show in the left.
-     */
-    left: PropTypes.element,
+const Header = props => {
+  const {
+    left,
+    right
+  } = props;
 
-    /**
-     * The element (an ActionIcon) that this element will show in the right.
-     */
-    right: PropTypes.element,
-  };
+  const logo = Logo({
+    size: Logo.BIG
+  });
 
-  static defaultProps = {
-    left: null,
-    right: null,
-  }
-
-  render() {
-    const { left, right } = this.props;
-    const logo = Logo({
-      size: Logo.BIG
-    });
-
-    return (
-      <div className={styles.container}>
-        <div className={styles.left}>
-          {left}
-        </div>
-        <div className={styles.center}>
-          {logo}
-        </div>
-        <div className={styles.right}>
-          {right}
-        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        {left}
       </div>
-    );
-  }
+      <div className={styles.center}>
+        {logo}
+      </div>
+      <div className={styles.right}>
+        {right}
+      </div>
+    </div>
+  );
 }
+
+Header.propTypes = {
+  /**
+   * The element (an ActionIcon) that this element will show in the left.
+   */
+  left: PropTypes.element,
+
+  /**
+   * The element (an ActionIcon) that this element will show in the right.
+   */
+  right: PropTypes.element,
+};
+
+Header.defaultProps = {
+  left: null,
+  right: null,
+}
+
+export default Header;
