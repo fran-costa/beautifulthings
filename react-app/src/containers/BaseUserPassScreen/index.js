b36315d8947b07b575dc936d8adc4c1bf823b32a
Francisco Costa <francisco.costa@unixono.com>
2018-07-16 12:12:31 -0300

    Add styles and refactor BaseUserPassScreen container

--- react-app/src/containers/BaseUserPassScreen/index.js
+++ react-app/src/containers/BaseUserPassScreen/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './index.module.scss';
-
 import InputText from 'components/InputText';
 import Logo from 'components/Logo';
+
+import styles from './index.module.scss';
 
 export default class BaseUserPassScreen extends React.PureComponent {
   static propTypes = {
@@ -13,16 +13,10 @@ export default class BaseUserPassScreen extends React.PureComponent {
     children: PropTypes.element.isRequired,
   }
 
-  static defaultProps = { onPasswordEnter: null }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+  state = {
+    username: '',
+    password: '',
+  };
 
   componentDidMount() {
     this._usernameInput.focus();
@@ -31,35 +25,73 @@ export default class BaseUserPassScreen extends React.PureComponent {
   _setUsernameInputRef = element => this._usernameInput = element;
   _setPasswordInputRef = element => this._passwordInput = element;
 
-  _notifyChanges = () => this.props.onChanges(this.state.username, this.state.password);
+  _notifyChanges = () => {
+    const { username, password } = this.state;
+
+    this.props.onChanges(username, password);
+  }
 
   _handleUsernameChange = username => this.setState({ username }, this._notifyChanges);
   _handlePasswordChange = password => this.setState({ password }, this._notifyChanges);
 
   _handleUsernameEnter = () => this._passwordInput.focus();
-  _handlePasswordEnter = () => this.props.onPasswordEnter ? this.props.onPasswordEnter() : null;
+  _handlePasswordEnter = () => {
+    const { onPasswordEnter } = this.props;
+
+    if (onPasswordEnter) onPasswordEnter();
+  }
+
+  _getLogo() {
+    return Logo({ size: Logo.BIG });
+  }
+
+  _getUsernameInput() {
+    return (
+      <InputText
+        type={InputText.TEXT}
+        label="Username:"
+        onChange={this._handleUsernameChange}
+        onEnter={this._handleUsernameEnter}
+        ref={this._setUsernameInputRef}
+      />
+    );
+  }
+
+  _getPasswordInput() {
+    return (
+      <InputText
+        type={InputText.PASSWORD}
+        label="Password:"
+        onChange={this._handlePasswordChange}
+        onEnter={this._handlePasswordEnter}
+        ref={this._setPasswordInputRef}
+      />
+    );
+  }
 
   render() {
+    const logo = this._getLogo();
+    const usernameInput = this._getUsernameInput();
+    const passwordInput = this._getPasswordInput();
+
     return (
       <div className={styles.container}>
-        <Logo size={Logo.BIG} />
-        <InputText
-          type={InputText.TEXT}
-          label="Username: "
-          placeholder="Username"
-          onChange={this._handleUsernameChange}
-          onEnter={this._handleUsernameEnter}
-          ref={this._setUsernameInputRef}
-        />
-        <InputText
-          type={InputText.PASSWORD}
-          label="Password: "
-          placeholder="Password"
-          onChange={this._handlePasswordChange}
-          onEnter={this._handlePasswordEnter}
-          ref={this._setPasswordInputRef}
-        />
-        {this.props.children}
+        <div className={styles.mainContainer}>
+          <div className={styles.logoContainer}>
+            {logo}
+          </div>
+          <div className={styles.formContainer}>
+            <div className={styles.usernameInputContainer}>
+              {usernameInput}
+            </div>
+            <div>
+              {passwordInput}
+            </div>
+            <div>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
