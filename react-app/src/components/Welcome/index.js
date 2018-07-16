a5233d434904ac559babb8ce5060ebb70a8751a9
Francisco Costa <francisco.costa@unixono.com>
2018-07-16 18:32:56 -0300

    Add styles and refactor Welcome component

--- react-app/src/components/Welcome/index.js
+++ react-app/src/components/Welcome/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import Arrow from './Arrow.svg';
+import styles from './index.module.scss';
+
 export default class Welcome extends React.PureComponent {
   render() {
     return (
-      <div>
-        What's your beautiful thing today?
+      <div className={styles.container}>
+        <div />
+        <div className={styles.messageContainer}>
+          What’s your beautiful thing today?
+        </div>
+        <div>
+          <img
+            src={Arrow}
+            alt=""
+          />
+        </div>
       </div>
     );
   }
