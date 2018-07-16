08385c243d4f5c6748b9967cd750625a76437179
Francisco Costa <francisco.costa@unixono.com>
2018-07-16 18:35:24 -0300

    Refactor Welcome component as a functional component

--- react-app/src/components/Welcome/index.js
+++ react-app/src/components/Welcome/index.js
@@ -3,21 +3,21 @@ import React from 'react';
 import Arrow from './Arrow.svg';
 import styles from './index.module.scss';
 
-export default class Welcome extends React.PureComponent {
-  render() {
-    return (
-      <div className={styles.container}>
-        <div />
-        <div className={styles.messageContainer}>
-          What’s your beautiful thing today?
-        </div>
-        <div>
-          <img
-            src={Arrow}
-            alt=""
-          />
-        </div>
+const Welcome = () => {
+  return (
+    <div className={styles.container}>
+      <div />
+      <div className={styles.messageContainer}>
+        What’s your beautiful thing today?
       </div>
-    );
-  }
+      <div>
+        <img
+          src={Arrow}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
+
+export default Welcome;
