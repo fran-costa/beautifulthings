3fa0daca025f2479265d29a559aa7c5af4d47059
Francisco Costa <francisco.costa@unixono.com>
2018-07-16 10:06:07 -0300

    Add styles and refactor Logo component

--- react-app/src/components/Logo/index.js
+++ react-app/src/components/Logo/index.js
@@ -1,10 +1,10 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import PlaceholderLogo from './PlaceholderLogo.svg';
+import AppLogo from './AppLogo.svg';
 import styles from './index.module.scss';
 
-export default class Logo extends PureComponent {
+export default class Logo extends React.PureComponent {
   static SMALL = 'small';
   static BIG = 'big';
 
@@ -12,14 +12,19 @@ export default class Logo extends PureComponent {
     /**
      * The size for the logo. Possible values: 'small' or 'big'
      */
-    size: PropTypes.oneOf([Logo.SMALL, Logo.BIG]).isRequired,
+    size: PropTypes.oneOf([
+      Logo.SMALL,
+      Logo.BIG
+    ]).isRequired,
   };
 
   render() {
+    const style = this.props.size === Logo.SMALL ? styles.small : styles.big;
+
     return (
       <img
-        className={(this.props.size === Logo.SMALL) ? styles.small : styles.big}
-        src={PlaceholderLogo}
+        className={style}
+        src={AppLogo}
         alt=""
       />
     );
