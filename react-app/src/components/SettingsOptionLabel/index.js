bb2b5855f5951fc1ba73d1312f680998740b8ac6
Francisco Costa <francisco.costa@unixono.com>
2018-07-17 17:08:08 -0300

    Refactor SettingsOptionLabel as a functional component

--- react-app/src/components/SettingsOptionLabel/index.js
+++ react-app/src/components/SettingsOptionLabel/index.js
@@ -3,46 +3,20 @@ import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
-export default class SettingsOptionLabel extends React.PureComponent {
-  static NOTIFICATION = 'notification';
-  static SIGNOUT = 'signout';
+const SettingsOptionLabel = props => {
+  const {
+    icon,
+    text,
+    onClick,
+    children,
+  } = props;
 
-  static propTypes = {
-    /**
-     * The icon to display next to the label text
-     */
-    icon: PropTypes.oneOf([
-      SettingsOptionLabel.NOTIFICATION,
-      SettingsOptionLabel.SIGNOUT,
-    ]).isRequired,
-
-    /**
-     * The label text
-     */
-    text: PropTypes.string.isRequired,
-
-    /**
-     * An optional function to call when element is tapped
-     */
-    onClick: PropTypes.func,
-
-    /**
-     * An optional children to display
-     */
-    children: PropTypes.element,
-  };
-
-  static defaultProps = {
-    onClick: null,
-    children: null,
-  };
-
-  _handleClick = () => {
-    if (this.props.onClick) this.props.onClick();
+  const _handleClick = () => {
+    if (onClick) onClick();
   }
 
-  _getIconStyle() {
-    switch (this.props.icon) {
+  function _getIconStyle() {
+    switch (icon) {
       case SettingsOptionLabel.NOTIFICATION:
         return styles.notifications;
       case SettingsOptionLabel.SIGNOUT:
@@ -52,25 +26,57 @@ export default class SettingsOptionLabel extends React.PureComponent {
     }
   }
 
-  render() {
-    const { text, children } = this.props;
-    const iconStyle = this._getIconStyle();
+  const iconStyle = _getIconStyle();
 
-    return (
-      <div
-        className={styles.container}
-        onClick={this._handleClick}
-      >
-        <div className={styles.labelContainer}>
-          <div className={iconStyle} />
-          <div>
-            {text}
-          </div>
-        </div>
+  return (
+    <div
+      className={styles.container}
+      onClick={_handleClick}
+    >
+      <div className={styles.labelContainer}>
+        <div className={iconStyle} />
         <div>
-          {children}
+          {text}
         </div>
       </div>
-    );
-  }
+      <div>
+        {children}
+      </div>
+    </div>
+  );
 }
+
+SettingsOptionLabel.NOTIFICATION = 'notification';
+SettingsOptionLabel.SIGNOUT = 'signout';
+
+SettingsOptionLabel.propTypes = {
+  /**
+   * The icon to display next to the label text
+   */
+  icon: PropTypes.oneOf([
+    SettingsOptionLabel.NOTIFICATION,
+    SettingsOptionLabel.SIGNOUT,
+  ]).isRequired,
+
+  /**
+   * The label text
+   */
+  text: PropTypes.string.isRequired,
+
+  /**
+   * An optional function to call when element is tapped
+   */
+  onClick: PropTypes.func,
+
+  /**
+   * An optional children to display
+   */
+  children: PropTypes.element,
+};
+
+SettingsOptionLabel.defaultProps = {
+  onClick: null,
+  children: null,
+};
+
+export default SettingsOptionLabel;
