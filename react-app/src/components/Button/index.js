22fb15f014c4666a28270716ae864c9f09c5e892
Francisco Costa <francisco.costa@unixono.com>
2018-07-17 13:39:37 -0300

    Refactor Button as a functional component

--- react-app/src/components/Button/index.js
+++ react-app/src/components/Button/index.js
@@ -3,64 +3,71 @@ import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
-export default class Button extends React.PureComponent {
-  static propTypes = {
-    /**
-     * The label or element the button will show.
-     */
-    children: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.element,
-    ]).isRequired,
+const Button = props => {
+  const {
+    children,
+    onClick,
+    disabled,
+    isSmall,
+  } = props;
 
-    /**
-     * The function to call when this button is clicked.
-     */
-    onClick: PropTypes.func.isRequired,
-
-    /**
-     * Whether this button is disabled or not.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * Whether this button is the small one or not. Default value: false
-     */
-    isSmall: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    disabled: false,
-    isSmall: false,
-  }
-
-  _handleClick = () => {
-    const { onClick, disabled } = this.props;
-
+  const _handleClick = () => {
     if (!disabled) onClick();
   };
 
-  render() {
-    const { disabled, isSmall, children } = this.props;
-    let style;
+  function _getStyle() {
     if (isSmall) {
-      if (disabled) style = styles.smallDisabled;
-      else style = styles.smallEnabled;
+      if (disabled) return styles.smallDisabled;
+      return styles.smallEnabled;
     } else {
-      if (disabled) style = styles.normalDisabled;
-      else style = styles.normalEnabled;
+      if (disabled) return styles.normalDisabled;
+      return styles.normalEnabled;
     }
-
-    return (
-      <div className={style}>
-        <button
-          className={styles.button}
-          disabled={disabled}
-          onClick={this._handleClick}
-        >
-          {children}
-        </button>
-      </div>
-    );
   }
+
+  const style = _getStyle();
+
+  return (
+    <div className={style}>
+      <button
+        className={styles.button}
+        disabled={disabled}
+        onClick={_handleClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
 }
+
+Button.propTypes = {
+  /**
+   * The label or element the button will show.
+   */
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+  ]).isRequired,
+
+  /**
+   * The function to call when this button is clicked.
+   */
+  onClick: PropTypes.func.isRequired,
+
+  /**
+   * Whether this button is disabled or not.
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * Whether this button is the small one or not. Default value: false
+   */
+  isSmall: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  disabled: false,
+  isSmall: false,
+};
+
+export default Button;
