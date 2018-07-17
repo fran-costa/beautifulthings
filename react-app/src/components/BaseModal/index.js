85d8cf088c6d11379c811ce62082d0d05fdbac0d
Francisco Costa <francisco.costa@unixono.com>
2018-07-17 16:27:11 -0300

    Refactor BaseModal as a functional component

--- react-app/src/components/BaseModal/index.js
+++ react-app/src/components/BaseModal/index.js
@@ -1,41 +1,44 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
-export default class BaseModal extends PureComponent {
-  static propTypes = {
-    /**
-     * Whenever the modal is visible or not
-     */
-    visible: PropTypes.bool.isRequired,
+const BaseModal = props => {
+  const {
+    visible,
+    leftModal,
+    children,
+  } = props;
 
-    /**
-     * Whenever the modal is displayed on the left or if
-     * is displayed as a floating modal (default behaviour)
-     */
-    leftModal: PropTypes.bool,
+  if (!visible) return null;
 
-    /**
-     * The element to use as the modal content
-     */
-    children: PropTypes.element.isRequired
-  };
-
-  static defaultProps = { leftModal: false }
-
-  render() {
-    if (!this.props.visible) return null;
-
-    const modalStyle = this.props.leftModal ? styles.leftModal : styles.floattingModal;
-
-    return (
-      <div>
-        <div className={styles.opacityLayer} />
-        <div className={modalStyle}>
-          {this.props.children}
-        </div>
+  const modalStyle = leftModal ? styles.leftModal : styles.floattingModal;
+  return (
+    <div>
+      <div className={styles.opacityLayer} />
+      <div className={modalStyle}>
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+BaseModal.propTypes = {
+  /**
+   * Whenever the modal is visible or not
+   */
+  visible: PropTypes.bool.isRequired,
+
+  /**
+   * Whenever the modal is displayed on the left or if
+   * is displayed as a floating modal (default behaviour)
+   */
+  leftModal: PropTypes.bool,
+
+  /**
+   * The element to use as the modal content
+   */
+  children: PropTypes.element.isRequired,
+};
+
+export default BaseModal;
