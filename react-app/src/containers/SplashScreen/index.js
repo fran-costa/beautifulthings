50294d89aff6b83bdef3b906f958d8d2593c991a
Francisco Costa <francisco.costa@unixono.com>
2018-07-26 15:22:03 -0300

    Integrate api functionality to SplashScreen

--- react-app/src/containers/SplashScreen/index.js
+++ react-app/src/containers/SplashScreen/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import api from 'api';
 
 import styles from './index.module.scss';
 
 export default class SplashScreen extends React.PureComponent {
+  static propTypes = {
+    onSignedIn: PropTypes.func.isRequired,
+    onNotSignedIn: PropTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    const { onSignedIn, onNotSignedIn } = this.props;
+
+    api.initSavedAccount()
+      .then(success => success ? onSignedIn() : onNotSignedIn());
+  }
+
   render() {
     return (
       <div className={styles.container}>
