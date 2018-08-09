26a05c025ef456ce3ebf2d7bbfca42ebbc4beca6
Francisco Costa <francisco.costa@unixono.com>
2018-08-09 14:55:47 -0300

    Improve AppRouter and add SignUpScreenContainer

--- react-app/src/AppRouter.js
+++ react-app/src/AppRouter.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
 
+import SignUpScreenContainer from 'containers/SignUpScreenContainer';
 import StartScreenContainer from 'containers/StartScreenContainer';
 
 const AppRouter = () => {
-  const _openSignUpScreen = () => window.location.hash = '/signup';
+  const routesNames = {
+    start: '/start',
+    signUp: '/signup',
+  }
+
+  const _openSignUpScreen = () => window.location.hash = routesNames.signUp;
+  const _openStartScreen = () => window.location.hash = routesNames.start;
 
   const _renderStartScreen = () => StartScreenContainer({
     onSignUp: _openSignUpScreen,
   });
 
+  const _renderSignUpScreen = () => SignUpScreenContainer({
+    onSignIn: _openStartScreen,
+  });
+
   return (
     <HashRouter>
       <div>
-        <Redirect exact path="/" to="/start" />
-        <Route exact path="/start" render={_renderStartScreen} />
+        <Redirect exact path="/" to={routesNames.start} />
+        <Route exact path={routesNames.start} render={_renderStartScreen} />
+        <Route exact path={routesNames.signUp} render={_renderSignUpScreen} />
       </div>
     </HashRouter>
   );
