a4a065a18922baf0fafa1eb1085fd1d860378d63
Francisco Costa <francisco.costa@unixono.com>
2018-08-09 14:19:58 -0300

    Add styles and improve LoadingModal

--- react-app/src/components/LoadingModal/index.js
+++ react-app/src/components/LoadingModal/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/fontawesome-free-solid';
@@ -7,29 +7,34 @@ import BaseModal from 'components/BaseModal';
 
 import styles from './index.module.scss';
 
-export default class LoadingModal extends PureComponent {
-  static propTypes = {
-    /**
-     * Whenever the modal is visible or not
-     */
-    visible: PropTypes.bool.isRequired,
+const LoadingModal = ({ visible, message }) => {
+  const children = (
+    <div className={styles.container}>
+      <div className={styles.spinner}>
+        <FontAwesomeIcon icon={faSpinner} pulse />
+      </div>
+      <div>
+        {message}
+      </div>
+    </div>
+  );
 
-    /**
-     * The message that the modal will show
-     */
-    message: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <BaseModal visible={this.props.visible}>
-        <div className={styles.container}>
-          <FontAwesomeIcon icon={faSpinner} pulse />
-          <div>
-            {this.props.message}
-          </div>
-        </div>
-      </BaseModal>
-    );
-  }
+  return BaseModal({
+    visible,
+    children,
+  });
 }
+
+LoadingModal.propTypes = {
+  /**
+   * Whenever the modal is visible or not
+   */
+  visible: PropTypes.bool.isRequired,
+
+  /**
+   * The message that the modal will show
+   */
+  message: PropTypes.string.isRequired,
+};
+
+export default LoadingModal;
