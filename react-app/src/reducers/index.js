cce0ad3ff5cece034269a0dcd8cf94513187f257
Francisco Costa <francisco.costa@unixono.com>
2018-08-20 13:43:47 -0300

    Clean the store when sign out

--- react-app/src/reducers/index.js
+++ react-app/src/reducers/index.js
@@ -4,10 +4,20 @@ import account from './account';
 import entries from './entries';
 import entriesByDate from './entriesByDate';
 
-const rootReducer = combineReducers({
+import { SIGN_OUT } from 'actions/account';
+
+const appReducer = combineReducers({
   account,
   entries,
   entriesByDate,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === SIGN_OUT) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+}
 
 export default rootReducer;
